1731ff68df90a3a7aafd67f8a5cc80c04f01bfca
samuel girgis <sg772@afsaccess1.njit.edu>
2017-02-08 20:37:47 -0500

    updated index

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,18 @@
+<?php
+$product_description = $_POST ['product_description'];
+$list_price = $_POST ['list_price'];
+$l = $_POST ['discount_percent'];
+$discount = $list_price * $l * .01;
+$discount_price = $list_price - $discount;
+
+$list_price_formatted = "$".number_format ($list_price, 2);
+$l_formatted = $l."%";
+$discount_formatted = "$".number_format ($discount, 2);
+$discount_price_formatted = "$".number_format ($discount_price, 2);
+$product_description_escaped = htmlspecialchars ($product_description);
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,22 +21,22 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description_escaped; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price_formatted; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $l_formatted; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_formatted; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price_formatted; ?></span><br>
     </main>
 </body>
 </html>
